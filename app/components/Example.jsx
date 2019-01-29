1d94b618886ec5edcab4dc5fb3a2adeb860a264b
pmanav2412 <mpatel2@albany.edu>
2019-01-29 01:11:40 -0500

    change

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -7,7 +7,7 @@ var React = require('react');
 var Example = () => {
     return(
         <div>
-            <h2>Example</h2>
+            <h2>Example!</h2>
             <p>This is Example!</p>
         </div>
         
