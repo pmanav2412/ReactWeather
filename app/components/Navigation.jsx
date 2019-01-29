4a594928fe90e7765238cda2dcef2f6ce7aaea64
pmanav2412 <mpatel2@albany.edu>
2019-01-29 10:40:50 -0500

    nav

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -6,11 +6,21 @@ var {Link, IndexLink}= require('react-router');
 
 var Navigation = () => {
     return (
-        <div>
-            <h2>This IS Navigation !</h2>
-            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>WeatherApp </IndexLink>
-            <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About </Link>
-            <Link to="/exam" activeClassName="active" activeStyle={{fontWeight:'bold'}}> EXAMPLE</Link>
+        <div className="top-bar">
+            <div className="menu">
+                <div className="top-bar-left">
+                    <ul>
+                        <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>WeatherApp </IndexLink></li>
+                    </ul>
+                    <ul>
+                        <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About </Link></li>
+                    </ul>
+                    <ul>
+                        <li><Link to="/exam" activeClassName="active" activeStyle={{fontWeight:'bold'}}> EXAMPLE</Link></li>
+                    </ul>
+                
+                </div>
+            </div>
         </div>
        
 
@@ -19,3 +29,33 @@ var Navigation = () => {
 
 //<---------------------------------------------------------------->
 module.exports=Navigation;
+
+Copy
+
+            
+            <Link to="/exam" activeClassName="active" activeStyle={{fontWeight:'bold'}}> EXAMPLE</Link>
+
+
+<div >
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+      <li class="menu-text">Site Title</li>
+      <li>
+        <a href="#">One</a>
+        <ul class="menu vertical">
+          <li><a href="#">One</a></li>
+          <li><a href="#">Two</a></li>
+          <li><a href="#">Three</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Two</a></li>
+      <li><a href="#">Three</a></li>
+    </ul>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu">
+      <li><input type="search" placeholder="Search"></li>
+      <li><button type="button" class="button">Search</button></li>
+    </ul>
+  </div>
+</div>
