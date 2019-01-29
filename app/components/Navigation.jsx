68d75f23772f85f66851e7544c3e6cf24c73065a
pmanav2412 <mpatel2@albany.edu>
2019-01-29 01:03:51 -0500

    example

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -10,7 +10,7 @@ var Navigation = () => {
             <h2>This IS Navigation !</h2>
             <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>WeatherApp</IndexLink>
             <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</Link>
-            <Link to="/exam" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examination</Link>
+            <Link to="/exam" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Example</Link>
         </div>
        
 
